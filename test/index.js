498298051b670de2ed9370a6df5a8ac444bad7e3
人間失格 <jacob@dnamicro.com>
2017-10-11 19:06:46 +0800

    updating test

--- test/index.js
+++ test/index.js
@@ -9,25 +9,33 @@ let PDF2PicInstance = new PDF2Pic({
 })
 
 describe('PDF2Pic', () => {
-    it("should convert pdf1 first page", function* () {
+    it("should convert pdf1 first page", function () {
         PDF2PicInstance.set("savedir", "./test/o/test_1")
-        yield PDF2PicInstance.convert("./test/docs/pdf1.pdf")
+        PDF2PicInstance.convert("./test/docs/pdf1.pdf").then(resolve => {
+            return assert(resolve.size > 0, "conversion is successful")
+        })
     })
     
-    it("should convert pdf1 second page", function* () {
+    it("should convert pdf1 second page", function () {
         PDF2PicInstance.set("savedir", "./test/o/test_2")
-         yield PDF2PicInstance.convert("./test/docs/pdf1.pdf", 2)
+        PDF2PicInstance.convert("./test/docs/pdf1.pdf", 2).then(resolve => {
+            return assert(resolve.size > 0, "conversion is successful")
+        })
     })
 
-    it("should convert pdf1 all pages", function* () { 
+    it("should convert pdf1 all pages", function () { 
         this.timeout(100000)
         PDF2PicInstance.set("savedir", "./test/o/test_3")
-        yield PDF2PicInstance.convertBulk("./test/docs/pdf1.pdf", -1)
+        PDF2PicInstance.convertBulk("./test/docs/pdf1.pdf", -1).then(resolve => {
+            return assert(resolve.length > 0, "conversion is successful")
+        })
     })
 
-    it("should convert pdf1 page 1 5 and 8", function* () { 
+    it("should convert pdf1 page 1 5 and 8", function () { 
         this.timeout(100000)
         PDF2PicInstance.set("savedir", "./test/o/test_4")
-        yield PDF2PicInstance.convertBulk("./test/docs/pdf1.pdf", [1,5,8])
+        PDF2PicInstance.convertBulk("./test/docs/pdf1.pdf", [1,5,8]).then(resolve => {
+            return assert(resolve.length > 0, "conversion is successful")
+        })
     })
 })
