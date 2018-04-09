f1285e69e2d4f2297e9004bc6804d384d8107ab4
yakovmeister <electro7bug@gmail.com>
2018-04-09 14:28:00 +0800

    added this.timeout to every it

--- test/index.js
+++ test/index.js
@@ -14,10 +14,10 @@ describe('PDF2Pic', function () {
     let savedir = `./test/output/${Math.random().toString(10).substring(7)}`
 
     pdf2pic.setOption('savedir', savedir)
-    this.timeout(1000000)
   })
 
   it('should convert pdf1 first page', function () {
+    this.timeout(1000000)
     return pdf2pic.convert('./test/docs/pdf1.pdf')
       .then(resolve =>
         expect(resolve.size).to.be.above(0)
@@ -25,27 +25,31 @@ describe('PDF2Pic', function () {
   })
 
   it('should convert pdf1 second page', function () {
+    this.timeout(1000000)
     return pdf2pic.convert('./test/docs/pdf1.pdf', 2)
       .then(resolve =>
         expect(resolve.size).to.be.above(0)
       )
   })
 
+  it('should convert pdf1 page 1, 5 and 8', function () {
+    this.timeout(1000000)
+    return pdf2pic.convertBulk('./test/docs/pdf1.pdf', [1, 5, 8])
+      .then(resolve =>
+        expect(resolve.length).to.be.equal(3)
+      )
+  })
+
   it("should convert pdf1 all pages", function () {
+    this.timeout(1000000)
     pdf2pic.convertBulk('./test/docs/pdf1.pdf', -1)
       .then(resolve =>
         expect(resolve.length).to.be.above(0)
       )
   })
 
-  it('should convert pdf1 page 1, 5 and 8', function () {
-    return pdf2pic.convertBulk('./test/docs/pdf1.pdf', [1, 5, 8])
-      .then(resolve =>
-        expect(resolve.length).to.be.equal(3)
-      )
-  })
-  
   it('should convert pdf1 first page to base64', function () {
+    this.timeout(1000000)
     return pdf2pic.convertToBase64('./test/docs/pdf1.pdf')
       .then(resolve =>
         expect(resolve.page).to.be.above(0)
