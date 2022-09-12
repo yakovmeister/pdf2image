2713391437c5c8468d44e76f744563d8779ccc5c
mskec <martin.skec@gmail.com>
2022-09-12 22:59:52 +0200

    Added bulkConvert batching
    - fixes https://github.com/yakovmeister/pdf2image/issues/54

--- src/utils/bulk/bulkConvert.ts
+++ src/utils/bulk/bulkConvert.ts
@@ -4,22 +4,13 @@ import { convertToStream } from "../converters/convertToStream";
 import { WriteImageResponse } from "../../types/writeImageResponse";
 import { ToBase64Response } from "../../types/toBase64Response";
 
-export async function bulkConvert(gm: Graphics, source: string, filePath: string | Buffer, pageNumber: number | number[] = 1, toBase64 = false): Promise<WriteImageResponse[] | ToBase64Response[]> {
-  if (pageNumber !== -1 && pageNumber < 1) {
-    throw new Error("Page number should be more than or equal 1");
-  }
-
-  const stream      = convertToStream(source, filePath);
-  const tempStream  = convertToStream(source, filePath);
-  const pageNumbers = Array.isArray(pageNumber) ? pageNumber : [pageNumber];
-
-  pageNumber = pageNumber === -1 ? await getPages(gm, tempStream) : pageNumbers;
-
+function convert(gm: Graphics, source: string, filePath: string | Buffer, pageNumber: number[], toBase64 = false): Promise<WriteImageResponse[] | ToBase64Response[]> {
+  const stream = convertToStream(source, filePath);
   const pages: (Promise<WriteImageResponse> | Promise<ToBase64Response>)[] = pageNumber.map(page => {
-    if (pageNumber < 1) {
+    if (page < 1) {
       throw new Error("Page number should be more than or equal 1");
     }
-  
+
     if (!!toBase64) {
       return gm.toBase64(stream, (page - 1));
     }
@@ -28,4 +19,18 @@ export async function bulkConvert(gm: Graphics, source: string, filePath: string
   });
 
   return Promise.all(pages);
+}
+
+export async function bulkConvert(gm: Graphics, source: string, filePath: string | Buffer, pageNumber: number | number[] = 1, toBase64 = false): Promise<WriteImageResponse[] | ToBase64Response[]> {
+  const pagesToConvert = pageNumber === -1
+    ? await getPages(gm, convertToStream(source, filePath))
+    : Array.isArray(pageNumber) ? pageNumber : [pageNumber];
+
+  const results = []
+  const batchSize = 10
+  for (let i = 0; i < pagesToConvert.length; i += batchSize) {
+    results.concat(...await convert(gm, source, filePath, pagesToConvert.slice(i, i + batchSize), toBase64))
+  }
+
+  return results
 }
