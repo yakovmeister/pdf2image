f510253d2aab90358ca131e85ed20e992d1f13f9
mskec <martin.skec@gmail.com>
2023-07-02 21:12:27 +0200

    Added convert types and additional tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,7 @@
 import chai, { expect } from "chai";
 import { mkdirsSync, readFileSync, writeFileSync } from "fs-extra";
 import gm from "gm";
+import path from 'path';
 import rimraf from "rimraf";
 import { fromBase64, fromBuffer, fromPath } from "../src/index";
 import { Graphics } from "../src/graphics";
@@ -18,12 +19,20 @@ describe("PDF2Pic Core", () => {
 
   const expectInfoToBeValid = (info: gm.ImageInfo, options) => {
     expect(info).to.haveOwnProperty('format');
-    expect(info.format).to.be.equal(options.format.toUpperCase());
+    if (options.format) {
+      expect(info.format).to.be.equal(options.format.toUpperCase());
+    } else {
+      expect(info.format).to.be.equal('PNG');
+    }
     expect(info).to.haveOwnProperty('size');
     expect(info.size).to.haveOwnProperty('width');
-    expect(info.size.width).to.be.equal(options.width);
+    if (options.width) {
+      expect(info.size.width).to.be.equal(options.width);
+    }
     expect(info.size).to.haveOwnProperty('height');
-    expect(info.size.height).to.be.equal(options.height);
+    if (options.height) {
+      expect(info.size.height).to.be.equal(options.height);
+    }
   }
 
   const expectImageResponseToBeValid = (response: WriteImageResponse, options: Options) => {
@@ -39,12 +48,25 @@ describe("PDF2Pic Core", () => {
     expect(response.page).to.be.a('number');
   }
 
+  const expectBase64ResponseToBeValid = (response: ToBase64Response) => {
+    expect(response).to.haveOwnProperty('base64');
+    expect(response.base64).to.be.a('string');
+    expect(response).to.haveOwnProperty('size');
+    expect(response.size).to.be.a('string');
+    expect(response).to.haveOwnProperty('page');
+    expect(response.page).to.be.a('number');
+  }
+
   before(() => {
     rimraf.sync("./dump/fromfiletest");
     rimraf.sync("./dump/frombuffertest");
     rimraf.sync("./dump/frombase64test");
 
     mkdirsSync("./dump/fromfiletest");
+  });
+
+  it('should use default options', async () => {
+    
   });
 
   it("should convert pdf to pic (file input, first page)", async () => {
@@ -56,11 +78,10 @@ describe("PDF2Pic Core", () => {
     }
 
     const convert = fromPath("./test/data/pdf1.pdf", options);
+    const imageResponse = await convert();
 
-    await convert() as WriteImageResponse;
-
+    expectImageResponseToBeValid(imageResponse, options);
     const info = await gm.identify("./dump/fromfiletest/test-1.1.png") as gm.ImageInfo;
-
     expectInfoToBeValid(info, options)
   });
 
@@ -74,11 +95,10 @@ describe("PDF2Pic Core", () => {
     const buffer = readFileSync("./test/data/pdf1.pdf");
 
     const convert = fromBuffer(buffer, options);
+    const imageResponse = await convert();
 
-    await convert() as WriteImageResponse;
-
+    expectImageResponseToBeValid(imageResponse, options);
     const info = await gm.identify("./dump/fromfiletest/test-1.1.png") as gm.ImageInfo;
-
     expectInfoToBeValid(info, options)
   });
 
@@ -92,11 +112,10 @@ describe("PDF2Pic Core", () => {
     const b64 = readFileSync("./test/data/pdf1.pdf", "base64");
 
     const convert = fromBase64(b64, options);
+    const imageResponse = await convert();
 
-    await convert() as WriteImageResponse;
-
+    expectImageResponseToBeValid(imageResponse, options);
     const info = await gm.identify("./dump/fromfiletest/test-1.1.png") as gm.ImageInfo;
-
     expectInfoToBeValid(info, options)
   });
 
@@ -109,13 +128,11 @@ describe("PDF2Pic Core", () => {
     }
 
     const convert = fromPath("./test/data/pdf1.pdf", options);
+    const base64Response = await convert(2, true);
 
-    const converted = (await convert(2, true) as ToBase64Response);
-
-    writeFileSync("./dump/fromfiletest/frombase64.png", Buffer.from(converted.base64, "base64"));
-
+    expectBase64ResponseToBeValid(base64Response)
+    writeFileSync("./dump/fromfiletest/frombase64.png", Buffer.from(base64Response.base64, "base64"));
     const info = await gm.identify("./dump/fromfiletest/frombase64.png") as gm.ImageInfo;
-
     expectInfoToBeValid(info, options)
   });
 
@@ -130,13 +147,11 @@ describe("PDF2Pic Core", () => {
     const buffer = readFileSync("./test/data/pdf1.pdf");
 
     const convert = fromBuffer(buffer, options);
+    const base64Response = await convert(2, { responseType: 'base64' });
 
-    const converted = (await convert(2, true) as ToBase64Response);
-
-    writeFileSync("./dump/fromfiletest/frombase64.png", Buffer.from(converted.base64, "base64"));
-
+    expectBase64ResponseToBeValid(base64Response)
+    writeFileSync("./dump/fromfiletest/frombase64.png", Buffer.from(base64Response.base64, "base64"));
     const info = await gm.identify("./dump/fromfiletest/frombase64.png") as gm.ImageInfo;
-
     expectInfoToBeValid(info, options)
   });
 
@@ -151,15 +166,34 @@ describe("PDF2Pic Core", () => {
     const b64 = readFileSync("./test/data/pdf1.pdf", "base64");
 
     const convert = fromBase64(b64, options);
+    const base64Response = await convert(2, { responseType: 'base64' });
 
-    const converted = (await convert(2, true) as ToBase64Response);
-
-    writeFileSync("./dump/fromfiletest/frombase64.png", Buffer.from(converted.base64, "base64"));
-
+    expectBase64ResponseToBeValid(base64Response)
+    writeFileSync("./dump/fromfiletest/frombase64.png", Buffer.from(base64Response.base64, "base64"));
     const info = await gm.identify("./dump/fromfiletest/frombase64.png") as gm.ImageInfo;
-
     expectInfoToBeValid(info, options)
   });
+
+  it("should convert pdf to pic (file input, bulk 2 pages)", async () => {
+    const gm = new Graphics();
+    const options = {
+      ...baseOptions,
+      format: "png",
+      width: 768,
+      height: 512,
+      saveFilename: "test-3"
+    }
+
+    const convert = fromPath("./test/data/pdf1.pdf", options);
+    const imageResponse = await convert.bulk([1, 2], { responseType: 'image' });
+
+    expect(imageResponse).to.be.an('array').that.has.lengthOf(2)
+    for (let i = 0; i < imageResponse.length; i++) {
+      expectImageResponseToBeValid(imageResponse[i], options)
+      const info = await gm.identify(`./dump/fromfiletest/test-3.${i + 1}.png`) as gm.ImageInfo;
+      expectInfoToBeValid(info, options)
+    }
+  }).timeout(7000);
 
   it("should convert pdf to pic (file input, bulk all pages)", async () => {
     const gm = new Graphics();
@@ -172,31 +206,14 @@ describe("PDF2Pic Core", () => {
     }
 
     const convert = fromPath("./test/data/pdf1.pdf", options);
-
-    const imageResponse = await convert.bulk(-1);
+    const imageResponse = await convert.bulk(-1, { responseType: 'image' });
 
     expect(imageResponse).to.be.an('array').that.has.lengthOf(9)
-    imageResponse.forEach(imageResponse => expectImageResponseToBeValid(imageResponse, options))
-
-    const info1 = await gm.identify("./dump/fromfiletest/test-3.1.png") as gm.ImageInfo;
-    const info2 = await gm.identify("./dump/fromfiletest/test-3.2.png") as gm.ImageInfo;
-    const info3 = await gm.identify("./dump/fromfiletest/test-3.3.png") as gm.ImageInfo;
-    const info4 = await gm.identify("./dump/fromfiletest/test-3.4.png") as gm.ImageInfo;
-    const info5 = await gm.identify("./dump/fromfiletest/test-3.5.png") as gm.ImageInfo;
-    const info6 = await gm.identify("./dump/fromfiletest/test-3.6.png") as gm.ImageInfo;
-    const info7 = await gm.identify("./dump/fromfiletest/test-3.7.png") as gm.ImageInfo;
-    const info8 = await gm.identify("./dump/fromfiletest/test-3.8.png") as gm.ImageInfo;
-    const info9 = await gm.identify("./dump/fromfiletest/test-3.9.png") as gm.ImageInfo;
-
-    expectInfoToBeValid(info1, options)
-    expectInfoToBeValid(info2, options)
-    expectInfoToBeValid(info3, options)
-    expectInfoToBeValid(info4, options)
-    expectInfoToBeValid(info5, options)
-    expectInfoToBeValid(info6, options)
-    expectInfoToBeValid(info7, options)
-    expectInfoToBeValid(info8, options)
-    expectInfoToBeValid(info9, options)
+    for (let i = 0; i < imageResponse.length; i++) {
+      expectImageResponseToBeValid(imageResponse[i], options)
+      const info = await gm.identify(`./dump/fromfiletest/test-3.${i + 1}.png`) as gm.ImageInfo;
+      expectInfoToBeValid(info, options)
+    }
   }).timeout(7000);
 
   it("should convert pdf to pic (buffer input, bulk all pages)", async () => {
@@ -212,31 +229,14 @@ describe("PDF2Pic Core", () => {
     const buffer = readFileSync("./test/data/pdf1.pdf");
 
     const convert = fromBuffer(buffer, options);
-
     const imageResponse = await convert.bulk(-1);
 
     expect(imageResponse).to.be.an('array').that.has.lengthOf(9)
-    imageResponse.forEach(imageResponse => expectImageResponseToBeValid(imageResponse, options))
-
-    const info1 = await gm.identify("./dump/fromfiletest/test-3.1.png") as gm.ImageInfo;
-    const info2 = await gm.identify("./dump/fromfiletest/test-3.2.png") as gm.ImageInfo;
-    const info3 = await gm.identify("./dump/fromfiletest/test-3.3.png") as gm.ImageInfo;
-    const info4 = await gm.identify("./dump/fromfiletest/test-3.4.png") as gm.ImageInfo;
-    const info5 = await gm.identify("./dump/fromfiletest/test-3.5.png") as gm.ImageInfo;
-    const info6 = await gm.identify("./dump/fromfiletest/test-3.6.png") as gm.ImageInfo;
-    const info7 = await gm.identify("./dump/fromfiletest/test-3.7.png") as gm.ImageInfo;
-    const info8 = await gm.identify("./dump/fromfiletest/test-3.8.png") as gm.ImageInfo;
-    const info9 = await gm.identify("./dump/fromfiletest/test-3.9.png") as gm.ImageInfo;
-
-    expectInfoToBeValid(info1, options)
-    expectInfoToBeValid(info2, options)
-    expectInfoToBeValid(info3, options)
-    expectInfoToBeValid(info4, options)
-    expectInfoToBeValid(info5, options)
-    expectInfoToBeValid(info6, options)
-    expectInfoToBeValid(info7, options)
-    expectInfoToBeValid(info8, options)
-    expectInfoToBeValid(info9, options)
+    for (let i = 0; i < imageResponse.length; i++) {
+      expectImageResponseToBeValid(imageResponse[i], options)
+      const info = await gm.identify(`./dump/fromfiletest/test-3.${i + 1}.png`) as gm.ImageInfo;
+      expectInfoToBeValid(info, options)
+    }
   }).timeout(7000);
 
   it("should convert pdf to pic (base64 input, bulk all pages)", async () => {
@@ -252,30 +252,15 @@ describe("PDF2Pic Core", () => {
     const b64 = readFileSync("./test/data/pdf1.pdf", "base64");
 
     const convert = fromBase64(b64, options);
+    const base64Responses = await convert.bulk(-1, { responseType: 'base64' });
 
-    const imageResponse = await convert.bulk(-1);
-
-    expect(imageResponse).to.be.an('array').that.has.lengthOf(9)
-    imageResponse.forEach(imageResponse => expectImageResponseToBeValid(imageResponse, options))
-
-    const info1 = await gm.identify("./dump/fromfiletest/test-3.1.png") as gm.ImageInfo;
-    const info2 = await gm.identify("./dump/fromfiletest/test-3.2.png") as gm.ImageInfo;
-    const info3 = await gm.identify("./dump/fromfiletest/test-3.3.png") as gm.ImageInfo;
-    const info4 = await gm.identify("./dump/fromfiletest/test-3.4.png") as gm.ImageInfo;
-    const info5 = await gm.identify("./dump/fromfiletest/test-3.5.png") as gm.ImageInfo;
-    const info6 = await gm.identify("./dump/fromfiletest/test-3.6.png") as gm.ImageInfo;
-    const info7 = await gm.identify("./dump/fromfiletest/test-3.7.png") as gm.ImageInfo;
-    const info8 = await gm.identify("./dump/fromfiletest/test-3.8.png") as gm.ImageInfo;
-    const info9 = await gm.identify("./dump/fromfiletest/test-3.9.png") as gm.ImageInfo;
-
-    expectInfoToBeValid(info1, options)
-    expectInfoToBeValid(info2, options)
-    expectInfoToBeValid(info3, options)
-    expectInfoToBeValid(info4, options)
-    expectInfoToBeValid(info5, options)
-    expectInfoToBeValid(info6, options)
-    expectInfoToBeValid(info7, options)
-    expectInfoToBeValid(info8, options)
-    expectInfoToBeValid(info9, options)
+    expect(base64Responses).to.be.an('array').that.has.lengthOf(9)
+    for (let i = 0; i < base64Responses.length; i++) {
+      expectBase64ResponseToBeValid(base64Responses[i])
+      const filename = `./dump/fromfiletest/test-3.b64.${i + 1}.png`
+      writeFileSync(filename, Buffer.from(base64Responses[i].base64, 'base64'))
+      const info = await gm.identify(filename) as gm.ImageInfo;
+      expectInfoToBeValid(info, options)
+    }
   }).timeout(7000);
 });
