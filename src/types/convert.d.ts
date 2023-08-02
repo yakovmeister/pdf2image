6d6a3b4c7ee8b65229d30e348b076647556e8d79
mskec <martin.skec@gmail.com>
2023-08-02 23:39:45 +0200

    Added responseType: buffer

--- src/types/convert.d.ts
+++ src/types/convert.d.ts
@@ -1,7 +1,6 @@
-import type { WriteImageResponse } from './convertResponse';
-import { ToBase64Response } from './convertResponse';
+import type { BufferResponse, ToBase64Response, WriteImageResponse } from './convertResponse';
 
-export type ResponseType = 'image' | 'base64'
+export type ResponseType = 'image' | 'base64' | 'buffer'
 export type ConvertOptions = boolean | {
   responseType: ResponseType
 }
@@ -11,12 +10,14 @@ export type Convert = {
   (pages: number, options: false | { responseType?: undefined }): Promise<WriteImageResponse>;
   (pages: number, options: true | { responseType: 'base64' }): Promise<ToBase64Response>;
   (pages: number, options: { responseType: 'image' }): Promise<WriteImageResponse>;
+  (pages: number, options: { responseType: 'buffer' }): Promise<BufferResponse>;
 
   bulk: {
     (pages?: number | number[], options?: undefined): Promise<WriteImageResponse[]>;
     (pages: number | number[], options: false | { responseType?: undefined }): Promise<WriteImageResponse[]>;
     (pages: number | number[], options: true | { responseType: 'base64' }): Promise<ToBase64Response[]>;
     (pages: number | number[], options: { responseType: 'image' }): Promise<WriteImageResponse[]>;
+    (pages: number | number[], options: { responseType: 'buffer' }): Promise<BufferResponse[]>;
   };
 
   setOptions: () => void;
