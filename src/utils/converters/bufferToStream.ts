a6254d764d81a926bc14cc6a564724a370d61901
yakovmeister <hireme@thefurious.dev>
2023-08-12 13:57:55 +0800

    use Readable.from

--- src/utils/converters/bufferToStream.ts
+++ src/utils/converters/bufferToStream.ts
@@ -2,12 +2,5 @@ import { ReadStream } from "fs";
 import { Readable } from "stream";
 
 export function bufferToStream(buffer: Buffer): ReadStream {
-  const readableInstanceStream = new Readable({
-    read() {
-      this.push(buffer);
-      this.push(null);
-    }
-  });
-
-  return readableInstanceStream as ReadStream;
+  return Readable.from(buffer) as ReadStream;
 }
